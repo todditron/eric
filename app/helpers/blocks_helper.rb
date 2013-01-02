de512893c82aad2a5c447ef570a3dc3ae28eba30
Taavo Smith <taavo@mac.com>
2013-01-02 16:16:43 -0500

    fix render_block on windows in devise views

--- app/helpers/blocks_helper.rb
+++ app/helpers/blocks_helper.rb
@@ -3,7 +3,8 @@ module BlocksHelper
     block = Block.find_by_name(name)
 
     if block
-      render block
+      # can't use `render block` because this breaks in devise views on windows
+      render partial: 'blocks/block', object: block
     else
       "render_block: Block not found \"#{name}\"" if Rails.env.development?
     end
